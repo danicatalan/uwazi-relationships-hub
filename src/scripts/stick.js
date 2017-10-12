d33e8c21a156858f2d200dde53f4f34d93e972bf
Dani Catalan <danicatalan@hotmail.com>
2017-10-12 17:43:54 +0200

    test sticky mobile

--- src/scripts/stick.js
+++ src/scripts/stick.js
@@ -1,6 +1,4 @@
-$(window).resize(function() {
-    if($(window).width() > 768) {
-        $(".source:not(.source-row):not(.source-connections-1)").stick_in_parent();
-        $(".group-wrapper:not(:first-child)").hide();
-    }
-});
+if($(window).width() > 768) {
+    $(".source:not(.source-row):not(.source-connections-1)").stick_in_parent();
+}
+$(".group-wrapper:not(:first-child)").hide();
