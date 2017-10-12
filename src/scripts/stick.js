382c1c84ceb9d14f973b483ca03e4a432af3d795
Dani Catalan <danicatalan@hotmail.com>
2017-10-12 17:40:59 +0200

    removing sticky on mobile

--- src/scripts/stick.js
+++ src/scripts/stick.js
@@ -1,2 +1,6 @@
-$(".source:not(.source-row):not(.source-connections-1)").stick_in_parent();
-$(".group-wrapper:not(:first-child)").hide();
+$(window).resize(function() {
+    if($(window).width() > 768) {
+        $(".source:not(.source-row):not(.source-connections-1)").stick_in_parent();
+        $(".group-wrapper:not(:first-child)").hide();
+    }
+});
